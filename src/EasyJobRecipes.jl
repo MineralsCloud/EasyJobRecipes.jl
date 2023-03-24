0de81f5b7ac11e135e6b8f6144e66d3f2f189a3d
singularitti <singularitti@outlook.com>
2023-03-23 21:12:12 -0400

    Add `TableHist` & `TablePlot`

--- src/EasyJobRecipes.jl
+++ src/EasyJobRecipes.jl
@@ -1,5 +1,42 @@
 module EasyJobRecipes
 
-# Write your package code here.
+using EasyJobs
+using Query
+using RecipesBase
+
+@userplot TableHist
+@recipe function f(obj::TableHist)
+    data = obj.args[begin]
+    attr = length(obj.args) == 2 ? obj.args[end] : :spent
+    values = @from item in data begin
+        @select getindex(item, attr)
+        @collect
+    end
+    if attr == :status
+        values = map(Int, values)
+    end
+    indices = eachindex(data)
+    framestyle --> :box
+    # lims --> extrema(values)
+    seriestype --> :histogram
+    legend_foreground_color --> nothing
+    return indices, values
+end
+
+@userplot TablePlot
+@recipe function f(obj::TablePlot)
+    data = obj.args[begin]
+    attr = length(obj.args) == 2 ? obj.args[end] : :spent
+    values = @from item in data begin
+        @select getindex(item, attr)
+        @collect
+    end
+    indices = eachindex(data)
+    framestyle --> :box
+    lims --> extrema(values)
+    seriestype --> :scatter
+    legend_foreground_color --> nothing
+    return indices, values
+end
 
 end
